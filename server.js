fb7e2fdcf132cd4493b1c0bc3510c5dd12025388
PradatiusD <danielprada2012@gmail.com>
2017-01-21 12:42:38 -0500

    server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-// server.js
 const express = require('express');
 const app = express();
-// Run the app by serving the static files
-// in the dist directory
+
+app.use('/images', express.static(__dirname + '/src/images'));
 app.use(express.static(__dirname + '/dist'));
+
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 4200);
